46d7029a39e0147385b4d097c2413daeb979f63f
Suvi <86911024+Gzxshg@users.noreply.github.com>
2024-08-11 16:19:55 +0800

    Add files via upload

--- API.py
+++ API.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[11]:
 
 
 import requests
 import os
 import json
+import re
+import warnings
 import ast
+token='UBIQ_TEAM159_MYk09vgECKnB'
 
 
 # In[2]:
@@ -20,6 +23,15 @@ def login():
       
 
 
+# In[ ]:
+
+
+def relogin():
+    str_login="python G:/UBIQuant/Code/API/main.py UBIQ_TEAM159 MYk09vgECKnB"
+    f=os.popen(str_login,"r")
+    return f.read()
+
+
 # In[3]:
 
 
@@ -30,7 +42,13 @@ def GetGameInfo():
     }
     response=requests.post(url,data=json.dumps(up_data))
     down_data=response.json()
-    return down_data
+    down_dict=eval(down_data)
+    if down_dict.get('status')=='Invalid User':
+        relogin()
+        return 0
+    else:
+        ret_data=down_dict.pop('status')
+        return ret_data
 
 
 # In[4]:
@@ -49,7 +67,8 @@ def Order(instrument,localtime,direction,price,volume):
     }
     response=requests.post(url,data=json.dumps(up_data))
     down_data=response.json()
-    return down_data
+    down_dict=eval(down_data)
+    return down_dict
 
 
 # In[5]:
@@ -66,7 +85,8 @@ def Cancel(instrument,localtime,index):
     }
     response=requests.post(url,data=json.dumps(up_data))
     down_data=response.json()
-    return down_data
+    down_dict=eval(down_data)
+    return down_dict
 
 
 # In[6]:
@@ -79,11 +99,12 @@ def GetActiveOrder():
     }
     response=requests.post(url,data=json.dumps(up_data))
     down_data=response.json()
-    return down_data
+    down_dict=eval(down_data)
+    return down_dict
     
 
 
-# In[8]:
+# In[4]:
 
 
 def GetTrade(instrument_name):
@@ -94,10 +115,11 @@ def GetTrade(instrument_name):
     }
     response=requests.post(url,data=json.dumps(up_data))
     down_data=response.json()
-    return down_data
+    down_dict=eval(down_data)
+    return down_dict
 
 
-# In[9]:
+# In[6]:
 
 
 def GetLimitOrderBook(instrument_name):
@@ -108,10 +130,21 @@ def GetLimitOrderBook(instrument_name):
     }
     response=requests.post(url,data=json.dumps(up_data))
     down_data=response.json()
-    return down_data
+    down_dict=eval(down_data)
+    def getbid_queue():
+        bid_queue=down_dict.get('bidprice')
+        return bid_queue
+    
+    def getask_queue():
+        ask_queue=down_dict.get('askprice')
+        return ask_queue
+    
+    ret_data=down_dict.pop('bidprice')
+    ret_data=ret_data.pop('askprice')
+    return ret_data
 
 
-# In[11]:
+# In[7]:
 
 
 def GetUserInfo():
@@ -121,10 +154,11 @@ def GetUserInfo():
     }
     response=requests.post(url,data=json.dumps(up_data))
     down_data=response.json()
-    return down_data
+    down_dict=eval(down_data)
+    return down_dict
 
 
-# In[12]:
+# In[9]:
 
 
 def GetInstrumentInfo():
@@ -134,5 +168,42 @@ def GetInstrumentInfo():
     }
     response=requests.post(url,data=json.dumps(up_data))
     down_data=response.json()
-    return down_data
+    down_dict=eval(down_data)
+    return down_dict
+
+
+# In[12]:
+
+
+def zero_divide(x,y):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        res=np.divide(x,y)
+    if hasattr(y,"__len__"):
+        res[y==0]=0
+    elif y==0:
+        res=0
+        
+    return res
+
+
+# In[13]:
+
+
+def score(instrument):
+    LOB_dict=GetLimitOrderBook(token,instrument)
+    userInfo_dict=GetUserInfo()
+    info_list=userInfo_dict.get('rows')
+    str=instrument
+    pattern=r'\d+'
+    n=re.findall(pattern,str)
+    target_dict=info_list[n]
+    target_volume=target_dict.get('target_volume')
+    remain_volume=target_dict.get('remain_volume')
+    trade_value=target_dict.get('trade_volume')
+    trade_volume=target_volume-remain_volume
+    vwap=zero_divide(trade_value,trade_volume)
+    twap=LOB_dict.get('lob')['twap']
+    return 1-vwap/twap
+    
 
